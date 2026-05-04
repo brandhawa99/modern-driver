3988e917a96fc65be3e52970e912427faf0ecfbb
brandhawa99 <baltej.randhawa0@gmail.com>
2026-05-03 22:33:35 -0700

    feat: remove unused TanStackRouterDevtools import and component from RootLayout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "../index.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -9,7 +8,6 @@ const RootLayout = () => {
     <div className="antialiasing bg-[--background] text-[--text] min-h-screen flex flex-col">
       <Header />
       <Outlet />
-      {/* <TanStackRouterDevtools /> */}
       <Footer />
     </div>
   );
