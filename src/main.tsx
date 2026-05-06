c706f657285de057bbe2a0363bd8a17e856f4e36
brandhawa99 <baltej.randhawa0@gmail.com>
2026-05-06 01:01:05 -0700

    feat: add defaultNotFoundComponent to router for improved error handling

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,14 @@ import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
+import CarDetailPageError from "./components/CarDetailPage/CarDetailPageError";
 
 // Create a new router instance
 const router = createRouter({
   routeTree,
   scrollRestoration: true,
   defaultViewTransition: true,
+  defaultNotFoundComponent: CarDetailPageError
 });
 
 // Register the router instance for type safety
